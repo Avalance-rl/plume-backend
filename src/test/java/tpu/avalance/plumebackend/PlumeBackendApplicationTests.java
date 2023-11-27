53f5c02af03799990e623d3fb10f2fc8c30f727b
Artyom <phenix392133@mail.ru>
2023-11-28 04:08:04 +0700

    initial commit

---
+++ src/test/java/tpu/avalance/plumebackend/PlumeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tpu.avalance.plumebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlumeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
